d6554896ad7a6f7b8bf36ce6f490e0677e1deda8
xuge <xuge@qq.com>
2022-07-08 12:56:03 +0800

    微信小程序功能菜单同步实现

--- service/service_wechat/src/main/java/com/xuge/ggkt/wechat/controller/MenuController.java
+++ service/service_wechat/src/main/java/com/xuge/ggkt/wechat/controller/MenuController.java
@@ -1,12 +1,16 @@
 package com.xuge.ggkt.wechat.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.xuge.ggkt.result.R;
 import com.xuge.ggkt.wechat.service.MenuService;
+import com.xuge.ggkt.wechat.utils.ConstantPropertiesUtil;
+import com.xuge.ggkt.wechat.utils.HttpClientUtils;
 import com.xuge.model.wechat.Menu;
 import com.xuge.vo.wechat.MenuVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,7 +30,48 @@ import java.util.List;
 public class MenuController {
   @Autowired
   private MenuService menuService;
-  //获取所有菜单，按照一级和二级菜单封装
+  @ApiOperation(value = "删除菜单")
+  @DeleteMapping("removeMenu")
+  public R removeMenu() {
+    menuService.removeMenu();
+    return R.ok(null);
+  }
+  @ApiOperation("同步菜单")
+  @GetMapping("syncMenu")
+  public R syncMenu(){
+    menuService.syncMenu();
+    return R.ok(null);
+  }
+
+  //获取access_token
+  @GetMapping("getAccessToken")
+  public R getAccessToken() {
+    try {
+      //拼接请求地址
+      StringBuffer buffer = new StringBuffer();
+      buffer.append("https://api.weixin.qq.com/cgi-bin/token");
+      buffer.append("?grant_type=client_credential");
+      buffer.append("&appid=%s");
+      buffer.append("&secret=%s");
+      //请求地址设置参数
+      String url = String.format(buffer.toString(),
+              ConstantPropertiesUtil.ACCESS_KEY_ID,
+              ConstantPropertiesUtil.ACCESS_KEY_SECRET);
+      //发送http请求
+      String tokenString = HttpClientUtils.get(url);
+      //获去access_token
+
+
+      JSONObject jsonObject = JSONObject.parseObject(tokenString);
+      String access_token = jsonObject.getString("access_token");
+      //返回
+      return R.ok(access_token);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return R.fail(null);
+    }
+  }
+    //获取所有菜单，按照一级和二级菜单封装
   @ApiOperation("获取所有菜单")
   @GetMapping("findMenuInfo")
   public R findMenuInfo() {
--- service/service_wechat/src/main/java/com/xuge/ggkt/wechat/service/impl/MenuServiceImpl.java
+++ service/service_wechat/src/main/java/com/xuge/ggkt/wechat/service/impl/MenuServiceImpl.java
@@ -1,12 +1,17 @@
 package com.xuge.ggkt.wechat.service.impl;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.xuge.ggkt.wechat.mapper.MenuMapper;
 import com.xuge.ggkt.wechat.service.MenuService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xuge.model.wechat.Menu;
 import com.xuge.vo.wechat.MenuVo;
+import me.chanjar.weixin.common.error.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -24,6 +29,8 @@ import java.util.stream.Collectors;
  */
 @Service
 public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements MenuService {
+  @Autowired
+  private WxMpService wxMpService;
   //获取所有菜单
   @Override
   public List<MenuVo> findMenuInfo() {
@@ -61,5 +68,59 @@ public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements Me
     QueryWrapper<Menu> qw = new QueryWrapper<>();
     qw.eq("parent_id",0);
     return  baseMapper.selectList(qw);
+  }
+  /**
+   * 说明：
+   * 自定义菜单最多包括3个一级菜单，每个一级菜单最多包含5个二级菜单。
+   * 一级菜单最多4个汉字，二级菜单最多8个汉字，多出来的部分将会以“...”代替。
+   * 创建自定义菜单后，菜单的刷新策略是，在用户进入公众号会话页或公众号profile页时，如果发现上一次拉取菜单的请求在5分钟以前，就会拉取一下菜单，如果菜单有更新，就会刷新客户端的菜单。测试时可以尝试取消关注公众账号后再次关注，则可以看到创建后的效果。
+   */
+  @Override
+  public void syncMenu()  {
+    //获取所有菜单
+    List<MenuVo> menuVoList = this.findMenuInfo();
+    //总菜单==json数组
+    JSONArray buttonList = new JSONArray();
+    //遍历所有一级菜单
+    for(MenuVo oneMenuVo : menuVoList) {
+      //创建json对象
+      JSONObject one = new JSONObject();
+      one.put("name", oneMenuVo.getName());
+      JSONArray subButton = new JSONArray();
+      //遍历的当前菜单的二级菜单
+      for(MenuVo twoMenuVo : oneMenuVo.getChildren()) {
+        JSONObject view = new JSONObject();
+        view.put("type", twoMenuVo.getType());
+        if(twoMenuVo.getType().equals("view")) {
+          view.put("name", twoMenuVo.getName());
+          view.put("url", "http://ggkt2.vipgz1.91tunnel.com/#"
+                  +twoMenuVo.getUrl());
+        } else {
+          view.put("name", twoMenuVo.getName());
+          view.put("key", twoMenuVo.getMeunKey());
+        }
+        subButton.add(view);
+      }
+      one.put("sub_button", subButton);
+      buttonList.add(one);
+    }
+    //菜单
+    JSONObject button = new JSONObject();
+    button.put("button", buttonList);
+    try {
+      //执行请求，数据同步
+      this.wxMpService.getMenuService().menuCreate(button.toJSONString());
+    } catch (WxErrorException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void removeMenu() {
+    try {
+      wxMpService.getMenuService().menuDelete();
+    } catch (WxErrorException e) {
+      e.printStackTrace();
+    }
   }
 }
